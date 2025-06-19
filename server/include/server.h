8f4b471e6a0bdc31378b0ff72932ce10c33a1f99
Aymen <abidaymen75@gmail.com>
2025-06-19 17:55:31 +0200

    feat : add server update function for commande queue

--- server/include/server.h
+++ server/include/server.h
@@ -24,7 +24,9 @@
     #define SUCCESS 0
     #define FAILURE 84
 
-typedef struct {
+typedef struct server_config_s server_config_t;
+
+typedef struct server_s {
     int fd;
     int port;
     int nb_clients;
@@ -35,9 +37,10 @@ typedef struct {
     int player_nb;
     struct pollfd pfds[NB_CONNECTION + 1];
     client_t clients[NB_CONNECTION + 1];
+    server_config_t *config; // Référence vers la configuration
 } server_t;
 
-typedef struct {
+struct server_config_s {
     int port;
     int width;
     int height;
@@ -46,16 +49,23 @@ typedef struct {
     int nb_clients;
     int freq;
     team_t *teams;
-} server_config_t;
+};
 
+// Fonctions du serveur
 void create_server(server_t *serv);
 void handle_client(server_t *serv);
 int launch_server(server_t *serv, server_config_t *config);
 void reset_server_clients(server_t *serv);
+
+// Fonctions de parsing
 int parse_args(int ac, char **av, server_config_t *config);
-int read_client_data(server_t *server, int i, char *buffer,
-    size_t buffer_size);
+
+// Fonctions de gestion des clients
+int read_client_data(server_t *server, int i, char *buffer, size_t buffer_size);
 void handle_client_message(server_t *server, int i, const char *buffer,
     server_config_t *config);
+
+// Fonctions utilitaires
+team_t *find_team(const char *name, server_config_t *config);
 
 #endif /* !SERVER_H_ */
